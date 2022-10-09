b0a52e41daafd986bce28d97a48d9969a95f4e8a
Christopher Williams <darkhemic@gmail.com>
2022-10-09 17:26:49 -0400

    Update webui.py

--- scripts/webui.py
+++ scripts/webui.py
@@ -210,7 +210,7 @@ def load_model_from_config(config, ckpt, verbose=False):
         print("unexpected keys:")
         print(u)
 
-    model.todevice()
+    model.to(device)
     model.eval()
     return model
 
