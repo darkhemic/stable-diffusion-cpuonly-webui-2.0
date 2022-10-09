87d902c2026a2b9091ca36f8568deff9f8324c10
Christopher Williams <darkhemic@gmail.com>
2022-10-09 16:49:24 -0400

    Update install_linux_sdco2.sh

--- install_linux_sdco2.sh
+++ install_linux_sdco2.sh
@@ -53,63 +53,6 @@ linux_setup_script () {
     bash -i ./webui.sh
 }
 
-# Checks to see which mode Ultimate Stable Diffusion is running in: STANDARD or OPTIMIZED
-# Then asks the user which mode they wish to use
-gradio_stable_diffusion_arguments () {
-    if [ "$1" = "customize" ]; then
-        printf "Do you want extra upscaling models to be run on the CPU instead of the GPU to save on VRAM at the cost of speed?\n"
-        select yn in "Yes" "No"; do
-            case $yn in
-                Yes ) printf "Setting extra upscaling models to use the CPU...\n"; sed -i 's/extra_models_cpu = False/extra_models_cpu = True/g' $DIRECTORY/scripts/relauncher.py; break;;
-                No ) printf "Extra upscaling models will run on the GPU. Continuing...\n"; sed -i 's/extra_models_cpu = True/extra_models_cpu = False/g' $DIRECTORY/scripts/relauncher.py; break;;
-            esac
-        done
-        printf "\n\n"
-        printf "Do you want for Stable Diffusion cpuonly webui 2.0 to automatically launch a new browser window or tab on first launch?\n"
-        select yn in "Yes" "No"; do
-            case $yn in
-                Yes ) printf "Setting Stable Diffusion cpuonly webui 2.0 to open a new browser window/tab at first launch...\n"; sed -i 's/open_in_browser = False/open_in_browser = True/g' $DIRECTORY/scripts/relauncher.py; break;;
-                No ) printf "Stable Diffusion cpuonly webui 2.0 will not open automatically in a new browser window/tab. Continuing...\n"; sed -i 's/open_in_browser = True/open_in_browser = False/g' $DIRECTORY/scripts/relauncher.py; break;;
-            esac
-        done
-        printf "\n\n"
-        printf "Do you want to run Ultimate Stable Diffusion in Optimized mode - Requires only 4GB of VRAM, but is significantly slower?\n"
-        select yn in "Yes" "No"; do
-            case $yn in
-                Yes ) printf "Setting Ultimate Stable Diffusion to run in Optimized Mode...\n"; sed -i 's/optimized = False/optimized = True/g' $DIRECTORY/scripts/relauncher.py; break;;
-                No ) printf "Ultimate Stable Diffusion will launch in Standard Mode. Continuing...\n"; sed -i 's/optimized = True/optimized = False/g' $DIRECTORY/scripts/relauncher.py; break;;
-            esac
-        done
-        printf "\n\n"
-        printf "Do you want to start Ultimate Stable Diffusion in Optimized Turbo mode - Requires more VRAM than regular optimized, but is faster (incompatible with Optimized Mode)?\n"
-        select yn in "Yes" "No"; do
-            case $yn in
-                Yes ) printf "Stable Diffusion cpuonly webui 2.0 to run in Optimized Turbo mode...\n"; sed -i 's/optimized_turbo = False/optimized_turbo = True/g' $DIRECTORY/scripts/relauncher.py; break;;
-                No ) printf "Stable Diffusion cpuonly webui 2.0 will launch in Standard Mode. Continuing...\n"; sed -i 's/optimized_turbo = True/optimized_turbo = False/g' $DIRECTORY/scripts/relauncher.py; break;;
-            esac
-        done
-        printf "\n\n"
-        printf "Do you want to create a public xxxxx.gradi.app URL to allow others to uses your interface? (Requires properly forwarded ports)\n"
-        select yn in "Yes" "No"; do
-            case $yn in
-                Yes ) printf "Setting Stable Diffusion cpuonly webui 2.0 to open a public share URL...\n"; sed -i 's/share = False/share = True/g' $DIRECTORY/scripts/relauncher.py; break;;
-                No ) printf "Setting Stable Diffusion cpuonly webui 2.0 to not open a public share URL. Continuing...\n"; sed -i 's/share = True/share = False/g' $DIRECTORY/scripts/relauncher.py; break;;
-            esac
-        done
-        printf "\n\nCustomization of Stable Diffusion cpuonly webui 2.0 is complete. Continuing...\n\n"
-    else
-        printf "\n\n########## GRADIO CUSTOMIZATION ##########\nPlease Note: These Arguments Only Affect The Gradio Interface Version Of The Stable Diffusion Webui.\n\n"
-        printf "Do you wish to customize the launch arguments for the Gradio Webui Interface?\n"
-        printf "(This will be where you select Optimized mode, auto open in browser, share to public, and more.)\n"
-        select yn in "Yes" "No"; do
-            case $yn in
-                Yes ) printf "Starting customization of Gradio Interface launch arguments...\n"; gradio_stable_diffusion_arguments customize; break;;
-                No ) printf "Maintaining current Gradio Interface launch arguments...\n"; break;;
-            esac
-        done
-    fi    
-}
-
 # Function to install and run the Ultimate Stable Diffusion fork
 stable_diffusion_cpuonly_webui_2 () {
     if [ "$1" = "initial" ]; then
